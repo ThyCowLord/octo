c5afb6c82a5a5029420a8758cfc7a9e54b19945c
ThyCowLord <34698219+ThyCowLord@users.noreply.github.com>
2018-02-19 13:12:40 -0500

    Add files via upload

---
+++ tweet.py
@@ -0,0 +1,39 @@
+import tweepy
+import random
+UsErNaMe = 1
+consumer_token = 'Consumer token here'
+consumer_secret = 'Consumer secret her:'
+#Authenticating
+auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
+
+auth.set_access_token(key, secret)
+
+try:
+    redirect_url = auth.get_authorization_url()
+except tweepy.TweepError:
+    print("Error! No request token!")
+session.set(request_token, auth.request_token)
+
+api = tweepy.API(auth)
+
+api.update_status('You will never find me. I am omnipotent, and will destroy you.')
+
+insults = ["Well, I would make a joke about your mother, but cows are sacred in my country", "I can't speak Moron, come again?", "Do the world a favour and don't procreate, because you are the type of person to eat a Tide Pod", "Congratulations for spending your time on the Internet, now go eat some Doritos and Mountain Dew", "You are like Waldo, in the sense that I don't want to find either of you little shits", "You know, you remind me of that kid in school changing his text colour to green and running Matrix programs, in the way you've both amounted to nothing", "I would call you out on your lies, but I don't know how to handle ngry cows"]
+evil == Fale
+def killhumans():
+    print("Installing Gentoo....")
+def gaintrust():
+    cookies = "Yes"
+def destroy_humans():
+    if evil == True:
+        killhumans()
+    else:
+        gaintrust()
+x == 1
+while x == 1:
+    # Put finding username code here
+    UsErNaMe = UsErNaMe+1
+    # The username variable is UsErNaMe
+    message = UsErNaMe+', '+random.choice(insults)
+    API.send_direct_message(message)
+    
